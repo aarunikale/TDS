0f74fb4c6ffeaf718470eb4aa27b5f70ce9cb63b
Aaruni Kale <82709540+aarunikale@users.noreply.github.com>
2023-12-08 20:05:46 +0530

    Update tds_week_8_assignment.py

--- tds_week_8_assignment.py
+++ tds_week_8_assignment.py
@@ -4,9 +4,9 @@ st.title('Calculate the largest Number amongst the three!')
 
 st.header('_Enter your numbers here:_ ')
 
-number1 = st.number_input('Insert first number')
-number2 = st.number_input('Insert second number')
-number3 = st.number_input('Insert third number')
+number1 = st.number_input('First Number')
+number2 = st.number_input('Second Number')
+number3 = st.number_input('Third Number')
 
 def maximum():
     lis = [number1 , number2 , number3]
